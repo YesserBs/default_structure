d40c052555316624974044949ca285870fe80222
Yesser <benselmayasser1@gmail.com>
2023-09-10 21:55:38 +0200

    This is the same default structure for a flutter project but with a dashboard

--- lib/settings/constant/config.dart
+++ lib/settings/constant/config.dart
@@ -4,7 +4,6 @@ class config {
   // colors
   static const Color primaryColor = Color(0xFFFF7F00);
   static const Color lightPrimaryColor = Color(0xFFFFFD0A2);
-
   // font families
   static const String font = 'OstrichSans';
 }
